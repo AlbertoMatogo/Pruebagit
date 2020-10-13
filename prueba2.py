9d2d394b3c01f7488bfe4dd05f9ecbdc480c1d92
Alberto Matogo <Albert.Mat.Man@gmail.com>
2020-10-13 13:47:20 +0200

    áñadido un archivo

---
+++ prueba2.py
@@ -0,0 +1,13 @@
+def saluda_idiomas(idioma):
+    if idioma=="Español":
+        saluda="Hola, ¿qué tal?"
+    
+    elif idioma=="Ingles":
+        saluda="Hi, How are yoy?"
+    
+    elif idioma=="Francés":
+        saluda="Salut, ¿ca va?"
+    
+    return saluda
+
+saluda_idiomas("Ingles")
